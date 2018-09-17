ec4068c3695de6bad5aa913bf25bd2fd9093ea14
Julianna Tetreault <juliannatetreault@icloud.com>
2018-09-17 03:40:59 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -60,10 +60,12 @@ def monopoly_with_fourth_tier
 		  {
 		    "mortage_value" => "$100"
 		  },
-		  :pennsylvania_railroad => {
+		  :pennsylvania_railroad => 
+		  {
 		    "mortgage_value" => "$200"
 		  },
-		  :b_and_o_railroad => {
+		  :b_and_o_railroad => 
+		  {
 		    "mortgage_value" => "$400"
 		  },
 		  :shortline => {
